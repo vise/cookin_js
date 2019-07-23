5ceecf6a913fdaaf252880e076dfbfd04d1c7aa9
Viggo Stenseth <vstenseth@spotify.com>
2019-07-23 23:49:49 +0200

    Update Cookin2.user.js

--- Cookin2.user.js
+++ Cookin2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cookin2
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  try to take over the world!
 // @author       You
 // @match        http://195.67.73.245/*
